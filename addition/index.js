d3539cc0e87dd5a02167569f3039b9dcc42f295c
michal jarnot <m.jarnot@yahoo.com>
2017-10-07 17:22:51 +0200

    Update addition script
    - make result log more user friendly

--- addition/index.js
+++ addition/index.js
@@ -33,6 +33,19 @@ myNetwork.train(trainingSet, {
   momentum: 0.9,
 })
 
-console.log(myNetwork.activate([0, 0])) // 0 + 0 = 0 (0.0145616980340058)
-console.log(myNetwork.activate([0.1, 0.2])) // 1 + 2 = 3 (0.3161242046611855)
-console.log(myNetwork.activate([0.5, 0.5])) // 1 + 5 = 6 (0.6145612045108657)
+/**
+ * Add integer1 to integer2 and console.log the result.
+ *
+ * @param integer1
+ * @param integer2
+ */
+function count(integer1, integer2) {
+  const result = Math.round(myNetwork.activate([integer1 / 10, integer2 / 10]) * 10)
+  console.log(`${integer1} + ${integer2} = ${result}`)
+}
+
+for (let i = 0; i < 10; i++) {
+  let integer1 = Math.floor(Math.random() * 10)
+  let integer2 = Math.round(Math.random() * (10 - integer1))
+  count(integer1, integer2)
+}
