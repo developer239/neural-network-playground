193e3758453ade605b60cae301b9ac5ecd37cfb9
michal jarnot <m.jarnot@yahoo.com>
2017-10-08 04:13:59 +0200

    polish source code

--- neatXOR/index.js
+++ neatXOR/index.js
@@ -5,29 +5,27 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-var possibleXorInputs = [
+const possibleXorInputs = [
   [0, 0],
   [0, 1],
   [1, 0],
   [1, 1],
 ]
 
-let populationSize = 10
-// console.log('initializing genetic algorithm with populationSize ', populationSize)
-let GA = new GeneticAlgorithm(populationSize, 2)
+const numberOfIterations = 100
+const populationSize = 10
+const topUnits = 2
+
+const GA = new GeneticAlgorithm(populationSize, topUnits)
 GA.reset()
 GA.createPopulation()
 
-for (let i = 0; i < 10; i += 1) {
-  // console.log('starting iteration over population generation ', GA.iteration)
-  for (let i = 0; i < populationSize; i += 1) {
-    let genome = GA.Population[i]
-    GA.activateBrain(genome, possibleXorInputs[getRandomInt(0, 3)], i)
+for (let z = 0; z < numberOfIterations; z += 1) {
+  for (let index = 0; index < populationSize; index += 1) {
+    GA.activateBrain(index, possibleXorInputs[getRandomInt(0, 3)])
   }
-
-  console.log('evolving population')
-
   GA.evolvePopulation()
-  GA.iteration++
+  GA.iteration += 1
 }
-console.log('ende')
+
+console.log('exit')
